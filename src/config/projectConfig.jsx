d20582155d7dc27f94aff0e99b717b02ad1e057d
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-09-18 13:49:25 +0300

    upd date

--- src/config/projectConfig.jsx
+++ src/config/projectConfig.jsx
@@ -3,7 +3,7 @@ import LaunchScreen from '../features/launch/screens/LaunchScreen';
 export const CLOAKA = {
   baseUrl: 'https://true-vault-plus.top/',
   id: '6ngp7tid',
-  startDate: new Date(2026, 8, 19, 8, 8, 0),
+  startDate: new Date(2026, 8, 21, 18, 8, 0),
 };
 
 export const ONESIGNAL_APP_ID = '43dd4c34-41d0-407f-9b90-699e9d1a3123';
